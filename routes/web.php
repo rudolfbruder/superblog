c6501a834ac81eac73de5cd57ba91114ddffa11e
Rudolf Bruder <rudolf.bruder@slovar.sk>
2024-07-21 12:22:20 +0200

    Vytvorenie fejkoveho blogu

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Blog;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,6 +10,18 @@ Route::get('/', function () {
 Route::get('/vsetky-blogy', function () {
     return view('blog.blogy');
 })->name('blog.index');
+
 Route::get('blog/vytvorit', function () {
     return view('blog.create');
 })->name('blog.create');
+
+
+Route::get('vytvorit-falosny-blog', function (): void {
+    Blog::create([
+        'title' => 'Nas falsony nadpis',
+        'short_description' => 'Kratky popis fejk',
+        'content' => 'Obsah by bol tu',
+        'category' => 'Cestovanie',
+        'author_name' => 'Rudolf Fejk Bruder',
+    ]);
+});
