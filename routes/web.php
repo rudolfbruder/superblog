fa3532fc7b9e3f8baf1291b443c70f70c8604fa6
Rudolf Bruder <rudolf.bruder@slovar.sk>
2024-07-21 12:20:40 +0200

    Flowbite a prve view

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/vsetky-blogy', function () {
+    return view('blog.blogy');
+})->name('blog.index');
